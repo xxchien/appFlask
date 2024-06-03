dbb1e8dc24a005291fe0f237e5b2244e6ed93ebd
pycharm <javichien@gmail.com>
2024-06-03 16:47:52 +0800

    test

---
+++ test.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/api')
+def api():
+
+    return 'api页面'
+@app.route('/api/test1')
+def test1():
+    return "test1"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
